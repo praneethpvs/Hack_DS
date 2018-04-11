8cce7ab2e48a876a7deb71dc634d4247588a94a2
praneethpvs <praneeth.bobby20@gmail.com>
2018-04-10 21:52:58 -0400

    Code to print the Linked List in reverse order.

---
+++ LinkedList/PrintReverse_LL.py
@@ -0,0 +1,28 @@
+"""
+ Print elements of a linked list in reverse order as standard output
+ head could be None as well for empty list
+ Node is defined as
+
+ class Node(object):
+
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+
+"""
+
+
+def ReversePrint(head):
+    reverse_list = []
+    if head is not None:
+        if head.next is None:
+            print head.data
+        else:
+            current = head
+            while (current is not None):
+                reverse_list.insert(0, current.data)
+                current = current.next
+            for val in reverse_list:
+                print val
+               
